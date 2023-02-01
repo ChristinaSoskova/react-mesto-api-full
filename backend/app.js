8dc96d200f5d48e1155d485478bf11d9c258336c
Kristina Soskova <soskovachristina@yandex.ru>
2023-02-01 17:20:36 +0300

    Исправлен локалхост

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 mongoose.set('strictQuery', false);
 
-const { PORT = 3000, DB = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 app.use(express.json());
 
 app.use(requestLogger);
